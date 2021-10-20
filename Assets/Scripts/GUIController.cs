5fe967e86fb1dee7f76395baf3d88d4ac466cc27
1 <slava.tnt.as58582@gmail.com>
2021-10-20 21:44:28 +0300

    Change UI font

--- Assets/Scripts/GUIController.cs
+++ Assets/Scripts/GUIController.cs
@@ -1,4 +1,5 @@
 using UnityEngine.UI;
+using TMPro;
 
 public class GUIController
 {
@@ -6,8 +7,8 @@ public class GUIController
 
     public static void UpdateGui()
     {
-        mscr.GameGui.GetComponentInChildren<Text>().text = $"{mscr.Players[mscr.PlayerStep].PlayerName}\nSteps:{mscr.Steps}";
-        mscr.Money.GetComponentInChildren<Text>().text = mscr.Players[mscr.PlayerStep].Money.ToString();
+        mscr.GameGui.GetComponentInChildren<TMP_Text>().text = $"{mscr.Players[mscr.PlayerStep].PlayerName}\nSteps:{mscr.Steps}";
+        mscr.Money.GetComponentInChildren<TMP_Text>().text = mscr.Players[mscr.PlayerStep].Money.ToString();
         mscr.PlayerColor.GetComponent<Image>().color = mscr.Players[mscr.PlayerStep].PlayerColor;
     }
 }
